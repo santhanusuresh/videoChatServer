125786ed3c58763e3b285e5b2f2f46fa008185e2
Santhanu <santhanu.s@shinehub.com.au>
2020-04-07 21:52:07 +0530

    last

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-
-app.use(cors());
 
 const users = {};
 io.on('connect', socket => {
